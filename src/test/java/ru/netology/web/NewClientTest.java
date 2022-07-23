85188b95f58ed0b2899fde3485e9177c9df8118d
Александр Сальников <alexander211981@mail.ru>
2022-07-24 01:23:43 +0700

    2nd commit

--- src/test/java/ru/netology/web/NewClientTest.java
+++ src/test/java/ru/netology/web/NewClientTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import static com.codeborne.selenide.Selenide.*;
@@ -8,6 +9,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class NewClientTest {
     @Test
     void shouldCreateNewValidActiveClient() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         RegistrationInfo validClient = DataGenerator.generateValidActiveClient();
         $("[name=login]").setValue(validClient.getLogin());
@@ -18,6 +20,7 @@ public class NewClientTest {
 
     @Test
     void shouldCreateNewValidBlockedClient() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         RegistrationInfo validClient = DataGenerator.generateValidBlockedClient();
         $("[name=login]").setValue(validClient.getLogin());
@@ -29,6 +32,7 @@ public class NewClientTest {
 
     @Test
     void shouldCreateClientWithIncorrectLogin() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         RegistrationInfo validClient = DataGenerator.generateClientWithIncorrectLogin();
         $("[name=login]").setValue(validClient.getLogin());
@@ -40,6 +44,7 @@ public class NewClientTest {
 
     @Test
     void shouldCreateClientWithIncorrectPassword() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         RegistrationInfo validClient = DataGenerator.generateClientWithIncorrectPassword();
         $("[name=login]").setValue(validClient.getLogin());
